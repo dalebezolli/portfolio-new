ac6df604874aff174678a62f90b961ce8b89d308
Dale Bezolli <dalebezolli@gmail.com>
2025-01-28 19:55:26 +0200

    Shorten outer layer width

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -3,7 +3,7 @@ import ProjectDisplay from "../components/ProjectDisplay";
 export function App() {
 	return (
 		<div className="bg-black text-white min-w-screen">
-			<div className="max-w-[1440px] h-screen bg-black mx-auto text-white flex gap-4 justify-center items-center">
+			<div className="max-w-[1080px] h-screen bg-black mx-auto text-white flex gap-4 justify-center items-center">
 				<ProjectDisplay name="Cool Project" img="test" className="grow" />
 				<div className="flex flex-col gap-4 grow-[0.3]">
 					<ProjectDisplay name="Cool Small Project" img="test" small={true} />
