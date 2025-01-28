e195e4103f99eabe1e227a3df92deab6e5d2c53a
Dale Bezolli <dalebezolli@gmail.com>
2025-01-28 20:09:33 +0200

    Transform ProjectDisplay background

--- src/components/ProjectDisplay.tsx
+++ src/components/ProjectDisplay.tsx
@@ -24,12 +24,21 @@ export default function ProjectDisplay({name, small=false, className=""}: Projec
 			rounded-xl
 			bg-zinc-900 font-primary
 			${className}`}>
+
+			<div className={`absolute inset-0 transition-all ${small ? 'group-hover:brightness-50' : ''}`}>
+				<div className="absolute top-[10%] left-[10%] w-[80px] h-[80px] rotate-3 bg-blue-600 rounded-md shadow-2xl"></div>
+				<div className="absolute top-[40%] left-[20%] w-[200px] h-[200px] rotate-12 bg-blue-400 rounded-md shadow-2xl"></div>
+				<div className="absolute top-[20%] right-[10%] w-[150px] h-[150px] rotate-45 bg-purple-400 rounded-md shadow-2xl"></div>
+				<div className="absolute bottom-[20%] right-[10%] w-[80px] h-[80px] rotate-[36deg] bg-purple-600 rounded-md shadow-2xl"></div>
+			</div>
+
 			<section className={`
 				p-8
 				flex flex-col gap-1
 				${small ? 'h-full justify-end' :''}
-				${small ? '' : 'bg-linear-60 from-zinc-950/50 to-zinc-600/50'}
+				${small ? '' : 'bg-linear-60 from-zinc-950/10 to-zinc-600/10'}
 				opacity-0 group-hover:opacity-100
+				backdrop-blur-2xl
 
 				${small ? '' :'translate-y-20 group-hover:translate-y-0'}
 
