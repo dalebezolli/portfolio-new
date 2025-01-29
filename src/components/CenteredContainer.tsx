b9d601774a3a11b87ab9f4554d1fffa05574e787
Dale Bezolli <dalebezolli@gmail.com>
2025-01-29 15:13:32 +0200

    Expose className of CenterContainer

--- src/components/CenteredContainer.tsx
+++ src/components/CenteredContainer.tsx
@@ -1,8 +1,8 @@
 import { PropsWithChildren } from "preact/compat";
 
-export function CenterContainer({ children }: PropsWithChildren) {
+export function CenterContainer({ className, children }: PropsWithChildren<{className?: string}>) {
 	return (
-		<div className="max-w-[1080px] mx-auto text-white px-2 sm:px-4">
+		<div className={`max-w-[1080px] mx-auto text-white px-2 sm:px-4 ${className}`}>
 			{children}
 		</div>
 	)
