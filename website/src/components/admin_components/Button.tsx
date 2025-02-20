09174834cf01bb1180a73642b3ae64973bbff22a
Dale Bezolli <dalebezolli@gmail.com>
2025-02-20 11:40:06 +0200

    Make button content not break

--- website/src/components/admin_components/Button.tsx
+++ website/src/components/admin_components/Button.tsx
@@ -37,7 +37,7 @@ export default function Button({text, icon, color, disabled=false, active=false,
 
 	return (
 		<button className={`
-			group px-4 py-2 flex gap-2 items-center rounded-xl font-bold
+			group px-4 py-2 flex gap-2 items-center rounded-xl font-bold w-max
 			${disabled ? "cursor-not-allowed" : "cursor-pointer"} transition-colors ${className}`} {...rest}>
 			{icon && <Icon icon={icon} colorFill={iconClassName} />}
 			{text}
