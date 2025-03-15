7c728dc5249519a8b5ed24f78b7e589d691e4249
Dale Bezolli <dalebezolli@gmail.com>
2025-03-15 22:09:11 +0200

    Make codeblock text on the same level as rest

--- website/mdx-components.tsx
+++ website/mdx-components.tsx
@@ -5,7 +5,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
 		h1: ({children}) => <h1 className="mb-5 text-3xl">{children}</h1>,
 		h2: ({children}) => <h2 className="mb-5 text-xl">{children}</h2>,
 		p: ({children}) => <p className="mb-5 text-lg text-justify text-zinc-200 [h1+*]:text-zinc-400">{children}</p>,
-		pre: ({children}) => <pre className="mb-5 p-4 text-sm rounded-xl bg-[#1a1b26] text-[#9aa5ce]">{children}</pre>,
+		pre: ({children}) => <pre className="-mx-4 mb-5 p-4 text-sm rounded-xl bg-[#1a1b26] text-[#9aa5ce]">{children}</pre>,
 		...components,
 	}
 }
