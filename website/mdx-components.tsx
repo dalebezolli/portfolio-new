77f2e5251bdd25c22cb91aa3f0e594cba07f4296
Dale Bezolli <dalebezolli@gmail.com>
2025-03-15 17:45:01 +0200

    Update margin for existing article elements

--- website/mdx-components.tsx
+++ website/mdx-components.tsx
@@ -2,9 +2,10 @@ import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
 	return {
-		h2: ({children}) => <h3 className="text-xl">{children}</h3>,
-		p: ({children}) => <p className="pb-5">{children}</p>,
-		pre: ({children}) => <pre className="p-4 rounded-xl bg-[#1a1b26] text-[#9aa5ce]">{children}</pre>,
+		h1: ({children}) => <h1 className="mb-5 text-3xl">{children}</h1>,
+		h2: ({children}) => <h2 className="mb-5 text-xl">{children}</h2>,
+		p: ({children}) => <p className="mb-5">{children}</p>,
+		pre: ({children}) => <pre className="mb-5 p-4 rounded-xl bg-[#1a1b26] text-[#9aa5ce]">{children}</pre>,
 		...components,
 	}
 }
