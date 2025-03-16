026fd239daa8c26684fdec4eb459b76499aafbe3
Dale Bezolli <dalebezolli@gmail.com>
2025-03-16 07:10:25 +0200

    Style lists

--- website/mdx-components.tsx
+++ website/mdx-components.tsx
@@ -5,7 +5,9 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
 		h1: ({children}) => <h1 className="mb-5 text-3xl">{children}</h1>,
 		h2: ({children}) => <h2 className="mt-15 md:mt-20 mb-5 md:mb-10 font-bold text-2xl text-fuchsia-400">{children}</h2>,
 		p: ({children}) => <p className="mb-5 text-lg [aside>*]:text-base text-justify text-zinc-200 [h1+*]:text-zinc-400">{children}</p>,
-		pre: ({children}) => <pre className="-mx-4 mb-5 p-4 text-sm rounded-xl bg-[#1a1b26] text-[#9aa5ce]">{children}</pre>,
+		pre: ({children}) => <pre className="-mx-4 mb-5 p-4 text-sm rounded-md bg-[#1a1b26] text-[#9aa5ce]">{children}</pre>,
+		ul: ({children}) => <ul className="mb-5 ml-[15px] list-disc marker:text-blue-400">{children}</ul>,
+		li: ({children}) => <li className="mb-3">{children}</li>,
 		...components,
 	}
 }
