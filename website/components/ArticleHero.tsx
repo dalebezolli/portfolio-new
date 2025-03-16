347834ee68098b3d14f9117b18dd5714da9561d6
Dale Bezolli <dalebezolli@gmail.com>
2025-03-16 06:14:04 +0200

    Make date accessible to screen readers

--- website/components/ArticleHero.tsx
+++ website/components/ArticleHero.tsx
@@ -8,12 +8,17 @@ export default function ArticleHero({title, publishedAt, children}: ArticleHeroP
 		year: "numeric"
 	});
 
+	const dateTimePublishedAt = publishedAtDate.toISOString().split("T")[0];
+
 	return (
 		<>
 			<div className="h-16 md:h-24"></div>
 			<h1 className="w-fit md:pb-2 text-2xl md:text-4xl font-heading font-semibold text-transparent bg-clip-text bg-linear-to-r from-white to-white/40 transition-colors">{title}</h1>
 			{children}
-			<p className="max-sm:text-sm text-zinc-400 italic">Last updated on {cleanPublishedAt}</p>
+			<p className="max-sm:text-sm text-zinc-400 italic">
+				Last updated on{" "}
+				<time dateTime={dateTimePublishedAt} className="max-lg text-zinc-200 not-italica font-bold">{cleanPublishedAt}</time>
+			</p>
 			<div className="h-12"></div>
 		</>
 	);
